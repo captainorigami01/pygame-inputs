a503a75a5e46932c612f976f9f29a7fd5616fbd1
captainorigami01 <86497366+captainorigami01@users.noreply.github.com>
2022-06-10 09:07:09 +0100

    Update button-sample.py

--- button-sample.py
+++ button-sample.py
@@ -3,21 +3,21 @@ import pygame
 
 pygame.init()
 clock = pygame.time.Clock()
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((500, 500))  # creates a window of 500 x 500 pixels
 
-myButton = Button(x=155, y=225, text="My Button", fontsize=18, font="calibri")
+myButton = Button(x=155, y=225, text="My Button", fontsize=18, font="calibri")  # sets a button to the myButton variable
 
 
 def update():
-  win.fill((60, 60, 60))
-  myButton.draw(win)
-  pygame.display.update()
+  win.fill((60, 60, 60))  # sets the background of the window in RGB
+  myButton.draw(win)  # draws the button to the window
+  pygame.display.update()  # updates the display
 
 run = True
 while run:
-  clock.tick(60)
+  clock.tick(60)  # sets the fps to a maximum 60
   for event in pygame.event.get():
-    if event.type == pygame.QUIT:
+    if event.type == pygame.QUIT:  # checks if the close button is pressed
       run = False
     if myButton.getPressed(event):
       print("Button pressed")
