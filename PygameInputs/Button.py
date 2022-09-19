be748e15c1be5efc8f3a9eb2d7d7373b06becf71
captainorigami01 <86497366+captainorigami01@users.noreply.github.com>
2022-09-19 13:24:50 +0100

    Add files via upload

--- PygameInputs/Button.py
+++ PygameInputs/Button.py
@@ -2,8 +2,6 @@ import pygame
 
 pygame.init()
 
-### Creates the Button class ###
-
 
 class Button(object):
     def __init__(self, x:int = 0, y:int = 0, width:int = 200, height:int = 50, text:str = "TEXT HERE", font:str = 'calibri', fontsize:int = 11, color:tuple = (0, 0, 0), background:tuple = (230, 230, 230), backgroundHover:tuple = (200, 200, 200), borderColour:tuple = (230, 230, 230), borderHoverColour:tuple = (200, 200, 200), borderWeight:int = 1, bold:bool = False, italic:bool = False):
@@ -28,34 +26,43 @@ class Button(object):
     def draw(self, window):
         self.isHover()
         if self.visible:
-            if self.hover:  # ifthe mouse is hovering show the hover state
+            if self.hover:
                 pygame.draw.rect(window, self.bgHover, (self.x, self.y, self.width, self.height))
                 if self.borderWidth > 0:
                     pygame.draw.rect(window, self.hBorder, (self.x, self.y, self.width, self.height), self.borderWidth)
-            else:  # Show the default state
+            else:
                 pygame.draw.rect(window, self.bg, (self.x, self.y, self.width, self.height))
                 if self.borderWidth > 0:
                     pygame.draw.rect(window, self.border, (self.x, self.y, self.width, self.height), self.borderWidth)
 
-            font = pygame.font.SysFont(self.font, self.fs, self.bold, self.italic)  # Sets the font
-            text = font.render(self.text, True, self.color)  # Sets the text
+            font = pygame.font.SysFont(self.font, self.fs, self.bold, self.italic)
+            text = font.render(self.text, True, self.color)
             text_rect = text.get_rect()
-            width = text_rect.width  # Text width
-            height = text_rect.height  # Text height
+            width = text_rect.width
+            height = text_rect.height
 
-            textX = (self.x + (self.width // 2)) - width // 2  # Centres the text horizontally (x) axis
-            textY = (self.y + (self.height // 2)) - height // 2  # Centres the text vertically (y) axis
+            textX = (self.x + (self.width // 2)) - width // 2
+            textY = (self.y + (self.height // 2)) - height // 2
 
-            window.blit(text, (textX, textY))  # displays the text
+            window.blit(text, (textX, textY))
 
     def isHover(self):
-        mouse = pygame.mouse.get_pos()  # gets the mouse position
+        mouse = pygame.mouse.get_pos()
 
-        if mouse[0] >= self.x and mouse[0] <= self.x + self.width:  # checks horizontal (x) axis
-            if mouse[1] >= self.y and mouse[1] <= self.y + self.height:  # checks vertical (y) axis
+        if mouse[0] >= self.x and mouse[0] <= self.x + self.width:
+            if mouse[1] >= self.y and mouse[1] <= self.y + self.height:
                 self.hover = True
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
             else:
                 self.hover = False
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
         else:
             self.hover = False
-        return self.hover  # returns the hover state
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        return self.hover
+
+    def getPressed(self, event):
+        if event.type == pygame.MOUSEBUTTONUP and self.hover:
+            return True
+        else:
+            return False
