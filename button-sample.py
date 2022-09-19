ceeb5afe2d286d4878c6873397749df943ff1907
captainorigami01 <86497366+captainorigami01@users.noreply.github.com>
2022-09-19 23:08:38 +0100

    Update button-sample.py

--- button-sample.py
+++ button-sample.py
@@ -1,4 +1,4 @@
-from pygameinputs.Button import Button
+from Button import Button, LinkButton, AppButton
 import pygame
 
 pygame.init()
@@ -6,12 +6,16 @@ clock = pygame.time.Clock()
 win = pygame.display.set_mode((500, 500))  # creates a window of 500 x 500 pixels
 
 myButton = Button(x=155, y=225, text="My Button", fontsize=18, font="calibri")  # sets a button to the myButton variable
-
+myAppButton = AppButton(app="C:\\Windows\\System32\\cmd.exe", text="CMD")
+myLinkButton = LinkButton(y=80, link="https://google.com/search?q=cute%20dogs", text="cute dogs")
 
 def update():
   win.fill((60, 60, 60))  # sets the background of the window in RGB
   myButton.draw(win)  # draws the button to the window
+  myAppButton.draw(win)
+  myLinkButton.draw(win)
   pygame.display.update()  # updates the display
+
 
 run = True
 while run:
@@ -20,6 +24,10 @@ while run:
     if event.type == pygame.QUIT:  # checks if the close button is pressed
       run = False  # Stops the while loop
     if myButton.getPressed(event):  # Checks if the button is pressed
-      print("Button pressed")  # Ouputs "Button pressed" if the button is pressed
+        print("Button pressed")  # Ouputs "Button pressed" if the button is pressed
+    if myAppButton.getPressed(event):
+      print("Opening cmd")
+    if myLinkButton.getPressed(event):
+      print("Opening google to find cute dogs")
   update()  # Runs the update function
 pygame.quit()  # Stops pygame and closes the window
