b30e66ffe9b59ba3999588cee0951a356dd94945
captainorigami01 <86497366+captainorigami01@users.noreply.github.com>
2023-03-22 22:11:31 +0000

    V 0.1.4
    Added border radius functionality to text boxes and buttons (except image buttons)

--- PygameInputs/Button.py
+++ PygameInputs/Button.py
@@ -10,7 +10,7 @@ class Button(object):
                  font: str = 'calibri', fontsize: int = 11, color: tuple = (0, 0, 0),
                  background: tuple = (230, 230, 230), backgroundHover: tuple = (200, 200, 200),
                  borderColour: tuple = (230, 230, 230), borderHoverColour: tuple = (200, 200, 200),
-                 borderWeight: int = 1, bold: bool = False, italic: bool = False):
+                 borderWeight: int = 1, bold: bool = False, italic: bool = False, borderRadius: int = -1):
         self.x = x
         self.y = y
         self.width = width
@@ -28,18 +28,21 @@ class Button(object):
         self.borderWidth = borderWeight
         self.visible = True
         self.hover = False
+        self.borderRadius = borderRadius
 
     def draw(self, window):
         if self.visible:
             self.isHover()
             if self.hover:
-                pygame.draw.rect(window, self.bgHover, (self.x, self.y, self.width, self.height))
+                pygame.draw.rect(window, self.bgHover, (self.x, self.y, self.width, self.height), 0, self.borderRadius)
                 if self.borderWidth > 0:
-                    pygame.draw.rect(window, self.hBorder, (self.x, self.y, self.width, self.height), self.borderWidth)
+                    pygame.draw.rect(window, self.hBorder, (self.x, self.y, self.width, self.height), self.borderWidth,
+                                     self.borderRadius)
             else:
-                pygame.draw.rect(window, self.bg, (self.x, self.y, self.width, self.height))
+                pygame.draw.rect(window, self.bg, (self.x, self.y, self.width, self.height), 0, self.borderRadius)
                 if self.borderWidth > 0:
-                    pygame.draw.rect(window, self.border, (self.x, self.y, self.width, self.height), self.borderWidth)
+                    pygame.draw.rect(window, self.border, (self.x, self.y, self.width, self.height), self.borderWidth,
+                                     self.borderRadius)
             if self.font.endswith(".ttf"):
                 font = pygame.font.Font(self.font, self.fs)
                 font.bold = self.bold
